e78990584611933c00150c1c3d20f680bc50b63f
Andrey Velikanov <andreyvelikanovv@gmail.com>
2025-05-11 02:02:25 +0400

    Remove limit pspayments list

--- api/handlers/pspayments.py
+++ api/handlers/pspayments.py
@@ -41,8 +41,7 @@ async def get_pspayments_settings(db: Database = Depends(get_db)):
             conn.row_factory = aiosqlite.Row
             cursor = await conn.execute("""
                 SELECT id, name, shop_id, description, is_enable 
-                FROM pspayments_settings 
-                WHERE is_enable = 1
+                FROM pspayments_settings
             """)
             settings = await cursor.fetchone()
 
